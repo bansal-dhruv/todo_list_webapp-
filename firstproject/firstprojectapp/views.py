2df84e004f735217e4b1dd2a17e06e19acae6cfd
Dhruv Bansal <dhruvbansal13999@gmail.com>
2019-10-03 21:55:35 +0530

    Update views.py

--- firstproject/firstprojectapp/views.py
+++ firstproject/firstprojectapp/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.forms import *
+
 from django.core.mail import send_mail
 
 from django.shortcuts import render, redirect
